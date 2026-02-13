f5e45784086da6b068dd475bcf92129cd9feb106
Sharanya <sharanyanaik2005@gmail.com>
2026-02-13 11:02:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+ipcMain.handle('open-file-dialog', async () => {
+  const result = await dialog.showOpenDialog({
+    properties: ['openFile'],
+    filters: [{ name: 'Images', extensions: ['jpg', 'png', 'jpeg'] }]
+  });
+
+  return result.filePaths;
+});
+
+app.whenReady().then(createWindow);
